c940822e1614c6ae0a1bf826465528199a4ea47b
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-27 17:31:31 +0200

    generate array 20 random people and conv to JSON

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ function randomChoiceArr(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+function choicePeople() {
+  const people = [];
+
+  for (let i = 0; i < 20; i++) {
+    const choiceGenders = randomChoiceArr(genders);
+    const choiceFirstName =
+      choiceGenders === 'female'
+        ? randomChoiceArr(firstNameFemale)
+        : randomChoiceArr(firstNameMale);
+    const choiceAge = Math.floor(Math.random() * (78 - 18 + 1) + 18);
+
+    const person = {
+      gender: choiceGenders,
+      firstName: choiceFirstName,
+      lastName: randomChoiceArr(lastName),
+      age: choiceAge,
+    };
+
+    people.push(person);
+  }
+  return JSON.stringify(people);
+}
+const randomPeoples = choicePeople();
+
+console.log(randomPeoples);
